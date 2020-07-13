325b2ac7415b930bb0881b957b35e743ac8db7a3
subramanya <subramanya115@gmail.com>
2020-07-13 19:31:28 +0530

    My Seventh Commit

--- app/components/Terms.js
+++ app/components/Terms.js
@@ -6,7 +6,7 @@ function Terms() {
   return (
     <Page title="Terms and Conditions">
       <h2>Our Terms &amp; Conditions</h2>
-      <p></p>
+      <div> </div>
       <div>
         <h1>* Users should SignUp with the registration form to get loggedin to the site and post their content with proper title.</h1>
         <h1>* Users have the availability to chat with other valid users who are registered.</h1>
