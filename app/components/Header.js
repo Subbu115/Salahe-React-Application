20bd817ae23bbf00c9284c00674f34bef47283a0
subramanya <subramanya115@gmail.com>
2020-07-13 19:37:23 +0530

    My 11th Commit

--- app/components/Header.js
+++ app/components/Header.js
@@ -10,11 +10,11 @@ function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
-        <h4 className="my-0 mr-md-auto font-weight-normal">
+        <h2 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
             Salahe
           </Link>
-        </h4>
+        </h2>
         {!props.staticEmpty ? headerContent : ""}
       </div>
     </header>
