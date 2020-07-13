ca300f0c60d8abd78e5ba73cfdc76bec0abb63a1
subramanya <subramanya115@gmail.com>
2020-07-13 19:40:29 +0530

    My 13th Commit

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -196,7 +196,7 @@ function HomeGuest() {
     <Page title="Welcome" wide={true}>
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
-          <h2 className="display-3">Heyy! Welcome Home</h2>
+          <h3 className="display-3">Heyy! Welcome Home</h3>
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again. Come on!! Here you go!!</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
